288a80f336dc229a7e0003fdd5207a2feb085984
Lucie Cupcakes <lucie_linux@protonmail.com>
2022-07-18 23:13:59 -0300

    Added 06-hooks-basics solution

---
+++ 06-hooks-basics/src/components/Ingredients/Ingredients.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    setUserIngredients(prevIngredients => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient }
+    ]);
+  };
+
+  const removeIngredientHandler = targetId => {
+    setUserIngredients(prevIngredients => prevIngredients.filter((ingredient) => ingredient.id !== targetId));
+  };
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search />
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
